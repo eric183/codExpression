e57cafe0544b6845a3855c6a6302f8a895827af7
kuangzhichen <kuangzhichen@gochinatv.com>
2016-05-24 13:03:41 +0800

    function WhoAmI(age, name, gender, fn) {

---
+++ 随机生成指定长度字符串.js
@@ -0,0 +1,15 @@
+
+/**
+ *@param {string} _string 传入指定范围的的字符串
+ *@param {number} mount   指定生成字符串的长度
+ */
+  
+function randomString(_string, mount) {
+    var _newString = '';
+
+    for (var i = 0; i < mount; i++) {
+        _newString += _string.charAt(Math.floor(Math.random() * _string.length));
+    }
+
+    return _newString;
+}
